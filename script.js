ef8a9a69a2e778b2da731feed28c57f5d7e479b3
Bryan <bryan.alessandromc.6061@gmail.com>
2024-08-21 10:32:14 -0400

    Own modal and javascript logic finished:  * Function to loop thorugh the array to display de books  * logic to open and close the modal depends of the event  * Styling the modal

--- script.js
+++ script.js
@@ -1,46 +1,99 @@
-// function Book (title, author, pages) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-
-//     this.sayBook = function() {
-//         console.log(`The "${title}" by ${author}, with ${pages} pages`);
-//     };
-// }
-
-// const book1 = new Book ('1984', 'George Orwell', '328');
-
-// console.log(Object.getPrototypeOf(book1));
-// book1.sayBook();
-
 const myLibrary = [];
-
-console.log(myLibrary.length === 0, "We dont have values for now"); // Debería mostrar true
 
 const bookName = document.getElementById('bookTitle');
 const authorName = document.getElementById('author');
 const numberPage = document.getElementById('pages');
 const button = document.getElementById('addBook');
+const containerBook = document.getElementById('bookContainer');
+const addBooksDialog = document.getElementById('addBtn');
+const closeTab = document.getElementById('closeModal');
+const modal = document.getElementById('modal');
 
+function openModal() {
+    addBooksDialog.addEventListener('click', function() {
+       modal.classList.add('modal--show');
+    })
+}
 
-function BookTest(title, author, pages) {
-    this.title = title;  
+function closeModal() {
+    closeTab.addEventListener('click', function() {
+        modal.classList.remove('modal--show');
+    })
+}
+
+openModal();
+closeModal();
+
+function Book(book, author, pages) {
+    this.book = book;  
     this.author = author;
     this.pages = pages;
     
     this.info = function() {
-        // const readStatus = this.read ? "read" : "not read yet";
-        console.log(`${this.title} by ${this.author} with ${this.pages} pages`);
+        containerBook.innerHTML = '';
+        console.log(`${book} ${author} ${pages}`);
+        const bookHeader = `<h1>${book}</h1>`;
+        const authorHeader = `<h2>${author}</h2>`;
+        const pagePara = `<p>${pages}</p>`;
+        const readBtn = `<button type="button" class="read-btn"><span>Read</span></button>`;
+        const div = `
+        <div class="book-cards">
+          ${bookHeader}
+          ${authorHeader}
+          ${pagePara}
+          ${readBtn}
+        </div>
+      `;
+      containerBook.innerHTML = div;
     };
 }
 
-button.addEventListener('click', function() {
-    const book = bookName.value.trim();
-    const author = authorName.value.trim();
-    const pages = numberPage.value.trim();
-    const x = new BookTest (book,author,pages);
+const book1 = new Book("book name", "author name", "number of pages: 999");
+book1.info();
 
-    myLibrary.push(x.info());
-    console.log(myLibrary);
-    console.log(myLibrary.includes(x.info()), "If the display it's true, the values that I push are inside the array");
-})
+function displayBooks() {
+    containerBook.innerHTML = '';
+
+    myLibrary.forEach(library => {
+        const bookHeader = `<h1><strong>Book name:</strong> ${library.book}</h1>`;
+        const authorHeader = `<h2><strong>By:</strong> ${library.author}</h2>`;
+        const pagePara = `<p><strong>Number of pages:</strong> ${library.pages}</p>`;
+        const readBtn = `<button type="button" class="read-btn"><span>Read</span></button>`;
+        const div = `
+        <div class="book-cards">
+          ${bookHeader}
+          ${authorHeader}
+          ${pagePara}
+          ${readBtn}
+        </div>
+      `;
+      containerBook.innerHTML += div;
+    });
+    document.querySelectorAll('.read-btn').forEach(btn => {
+        btn.addEventListener('click', function() {
+            btn.classList.toggle('unread-btn');
+            btn.classList.toggle('read-btn');
+        })
+    });
+}
+
+function addBookToLibrary() {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (bookName.value != '' && authorName.value != '' && numberPage.value != '') {
+            const book = bookName.value.trim();
+            const author = authorName.value.trim();
+            const pages = numberPage.value.trim();
+            const pushBooksInfo = new Book(book, author, pages);
+
+            myLibrary.push(pushBooksInfo);
+            displayBooks();
+            modal.classList.remove('modal--show');
+
+        } else {
+            alert('invalid inputs');
+        }
+    })
+}
+
+addBookToLibrary();
