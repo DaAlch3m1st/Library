ad9b1fcf7b0b30a7dfd97d8c3f56b45a5e873f30
Bryan <bryan.alessandromc.6061@gmail.com>
2024-08-21 20:34:15 -0400

    HTML:  * Footer with my GitHub CSS:  * Background color gradient to the two sections  * News font-family and colors JavaScript:  * When you click the read button the border-top changes according to the read status  * Add default container that show how the book will look like

--- script.js
+++ script.js
@@ -32,15 +32,19 @@ function Book(book, author, pages) {
     this.info = function() {
         containerBook.innerHTML = '';
         console.log(`${book} ${author} ${pages}`);
-        const bookHeader = `<h1>${book}</h1>`;
-        const authorHeader = `<h2>${author}</h2>`;
-        const pagePara = `<p>${pages}</p>`;
+        const header = `<h1>Book example</h1>`
+        const bookHeader = `<h1><em>Book:</em> <strong>${book}</strong></h1>`;
+        const authorHeader = `<h2><em>By:</em> <strong>${author}</strong></h2>`;
+        const pagePara = `<h3><em>Number of pages:</em> <strong>${pages}</strong></h3>`;
         const readBtn = `<button type="button" class="read-btn"><span>Read</span></button>`;
+        const readStatus = `<h4><em>Read Status:</em> <b>No</b></h4>`
         const div = `
         <div class="book-cards">
+          ${header}  
           ${bookHeader}
           ${authorHeader}
           ${pagePara}
+          ${readStatus}
           ${readBtn}
         </div>
       `;
@@ -48,32 +52,47 @@ function Book(book, author, pages) {
     };
 }
 
-const book1 = new Book("book name", "author name", "number of pages: 999");
+const book1 = new Book("1984", "George Orwell", "282");
 book1.info();
 
 function displayBooks() {
     containerBook.innerHTML = '';
 
     myLibrary.forEach(library => {
-        const bookHeader = `<h1><strong>Book name:</strong> ${library.book}</h1>`;
-        const authorHeader = `<h2><strong>By:</strong> ${library.author}</h2>`;
-        const pagePara = `<p><strong>Number of pages:</strong> ${library.pages}</p>`;
+        const bookHeader = `<h1><em>Book: </em> <strong>${library.book}</strong></h1>`;
+        const authorHeader = `<h2><em>By:</em> <strong>${library.author}</strong></h2>`;
+        const pagePara = `<h3><em>Number of pages:</em> <strong>${library.pages}</strong></h3>`;
         const readBtn = `<button type="button" class="read-btn"><span>Read</span></button>`;
+        const readStatus = `<h4><em>Read Status:</em> <b>No</b></h4>`
         const div = `
         <div class="book-cards">
           ${bookHeader}
           ${authorHeader}
           ${pagePara}
+          ${readStatus}
           ${readBtn}
         </div>
       `;
       containerBook.innerHTML += div;
     });
-    document.querySelectorAll('.read-btn').forEach(btn => {
+    document.querySelectorAll('.book-cards').forEach(bookCard => {
+        const btn = bookCard.querySelector('.read-btn');
+
         btn.addEventListener('click', function() {
             btn.classList.toggle('unread-btn');
             btn.classList.toggle('read-btn');
-        })
+            
+            // Alterna el borde superior del contenedor
+            if (btn.classList.contains('unread-btn')) {
+                bookCard.style.borderTop = '16px solid red';
+                btn.querySelector('span').textContent = 'Unread';
+                btn.querySelector('b').textContent = 'No';
+            } else {
+                bookCard.style.borderTop = '16px solid green';
+                btn.querySelector('span').textContent = 'Read';
+                btn.querySelector('b').textContent = 'Yes';
+            }
+        });
     });
 }
 
@@ -89,6 +108,9 @@ function addBookToLibrary() {
             myLibrary.push(pushBooksInfo);
             displayBooks();
             modal.classList.remove('modal--show');
+            bookName.value = '';
+            authorName.value = '';
+            numberPage.value = '';
 
         } else {
             alert('invalid inputs');
